532667a74bbc7fc202f8f10ea630f6eeb7517b05
egoraoooo <109613812+egoraoooo@users.noreply.github.com>
2026-06-26 01:42:56 +0600

    Add solution task01

---
+++ task01/task01.cs
@@ -0,0 +1,21 @@
+﻿namespace task01;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return false;
+        }
+
+        var filterchar = input.ToLower().Where(c => !char.IsWhiteSpace(c) && !char.IsPunctuation(c)).ToArray();
+
+        if (filterchar.Length == 0)
+        {
+            return false;
+        }
+
+        return filterchar.SequenceEqual(filterchar.Reverse());
+    }
+}
