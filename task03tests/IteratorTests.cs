4b4979933d29a2fda71a4550ba0e5f6a8d227b15
egoraoooo <109613812+egoraoooo@users.noreply.github.com>
2026-07-06 14:11:52 +0600

    Rename files

--- task03tests/IteratorTests.cs
+++ task03tests/IteratorTests.cs
@@ -1,9 +1,9 @@
-namespace task03tests;
+namespace IteratorTests;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
-using task03;
+using CustomCollection;
 
 public class IteratorTests
 {
